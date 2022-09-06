63f8ba6413fa479ecaba31a3e03a0b8412dec79e
SSD <samuserna2005@gmail.com>
2022-09-06 01:08:36 -0500

    Ejercicios de matrices con Java

---
+++ Matrices_01/Ejer1.java
@@ -0,0 +1,17 @@
+package Matrices_01;
+//Crear una matriz de 3x3 con los numeros del 1 al 9. Mostrar por pantalla,tal como aparece en la matriz
+
+public class Ejer1 {
+    public static void main(String[] args) {
+        int[][] matriz = new int[3][3];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                matriz[i][j] = (i * matriz.length) + j + 1;
+                System.out.print(matriz[i][j] + "|");
+            }
+            System.out.println();
+        }
+
+    }
+}
