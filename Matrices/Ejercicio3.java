ecde13e5adb64d7c38263337f646c62df08727d1
SSD <samuserna2005@gmail.com>
2022-09-06 09:47:36 -0500

    Ejercicios de matrices con Java

--- Matrices/Ejercicio3.java
+++ Matrices/Ejercicio3.java
@@ -42,10 +42,10 @@ public class Ejercicio3 {
 
     //Funcion para imprimir matrices
     public static void imprimirMatriz(int[][] matriz) {
-        for (int i = 0; i < matriz.length; i++) {
+        for (int[] ints : matriz) {
             for (int j = 0; j < matriz[0].length; j++) {
 
-                System.out.print(matriz[i][j] + "|");
+                System.out.print(ints[j] + "|");
             }
             System.out.println("");
         }
