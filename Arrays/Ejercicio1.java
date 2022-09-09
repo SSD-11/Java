c03603d0c6c7ef6a269c19bc60520b27855a10f3
SSD <samuserna2005@gmail.com>
2022-09-08 23:38:29 -0500

    Ejercicios de Arrays con Java

---
+++ Arrays/Ejercicio1.java
@@ -0,0 +1,17 @@
+package Arrays;
+//Crea un array que contenga los numeros del 1 al 100. Muestralos por pantalla.
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+        //Se crea El Array
+        int[] numeros = new int[100];
+
+        //Se recorre y se le asignan los valores
+        for(int i = 0; i < numeros.length;i++){
+            numeros[i] = i+1;
+            //Se imprimen los datos
+            System.out.println(numeros[i]);
+        }
+    }
+}
